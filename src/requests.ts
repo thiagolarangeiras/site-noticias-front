5a95ae8d42725e765649ac9489476db86b668340
Thiago <thiago.larangeira50@gmail.com>
2024-05-16 21:26:58 -0300

    requests

--- src/requests.ts
+++ src/requests.ts
@@ -1,11 +1,149 @@
+// const init: RequestInit = {
+//     method: "POST", // *GET, POST, PUT, DELETE, etc.
+//     mode: "cors", // no-cors, *cors, same-origin
+//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//     credentials: "same-origin", // include, *same-origin, omit
+//     headers: {
+//         "Content-Type": "application/json",
+//         // 'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     redirect: "follow", // manual, *follow, error
+//     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//     body: JSON.stringify(data), // body data type must match "Content-Type" header
+// };
+
+const url = "http://localhost:8080";
+
+type noticiaDtoPost = {
+    autor: string;
+    titulo: string;
+    texto: string;
+};
+
+type noticiaDtoPut = {
+    autor: string;
+    titulo: string;
+    texto: string;
+    data: string; 
+};
+
+type comentarioDtoPost = {
+    usuario: string;
+    texto: string
+};
+
+type comentarioDtoPut = {
+    texto: string;
+};
+
 export async function getNoticias() {
-    const response = await fetch("http://localhost:8080/noticias");
-    const noticias = await response.json();
-    return noticias;
+    const init: RequestInit = {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+    };
+    const response = await fetch(`${url}/noticias`, init);
+    return await response.json();
 }
 
-export async function getComentarios(noticiaId: number) {
-    const response = await fetch(`http://localhost:8080/noticias/${noticiaId}/comentarios`);
-    const comentarios = await response.json();
-    return comentarios;
+export async function postNoticia(dados: noticiaDtoPost) {
+    const init: RequestInit = {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: { "Content-Type": "application/json" },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(dados),
+    };
+    const response = await fetch(`${url}/noticias`, init);
+    return await response.json();
+}
+
+export async function putNoticia(idNoticia: number, dados: noticiaDtoPut) {
+    const init: RequestInit = {
+        method: "PUT",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: { "Content-Type": "application/json" },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(dados),
+    };
+    const response = await fetch(`${url}/noticias/${idNoticia}`, init);
+    return await response.json();
+}
+
+export async function deleteNoticia(idNoticia: number) {
+    const init: RequestInit = {
+        method: "DELETE",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+    };
+    const response = await fetch(`${url}/noticias/${idNoticia}`, init);
+    return await response.json();
+}
+
+export async function getComentarios(idNoticia: number) {
+    const init: RequestInit = {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+    };
+    const response = await fetch(`${url}/noticias/${idNoticia}/comentarios`, init);
+    return await response.json();
+}
+
+export async function postComentario(idNoticia: number, dados: comentarioDtoPost) {
+    const init: RequestInit = {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: { "Content-Type": "application/json" },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(dados),
+    };
+    const response = await fetch(`${url}/noticias/${idNoticia}/comentarios`, init);
+    return await response.json();
+}
+
+export async function putComentario(idNoticia: number, idComentario: number, dados: comentarioDtoPut) {
+    const init: RequestInit = {
+        method: "PUT",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: { "Content-Type": "application/json" },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(dados),
+    };
+    const response = await fetch(`${url}/noticias/${idNoticia}/comentarios/${idComentario}`, init);
+    return await response.json();
+}
+
+export async function deleteComentario(idNoticia: number, idComentario: number) {
+    const init: RequestInit = {
+        method: "DELETE",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+    };
+    const response = await fetch(`${url}/noticias/${idNoticia}/comentarios/${idComentario}`, init);
+    return await response.json();
 }
